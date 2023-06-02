07c0058befad1983fe169dfc6db2d0283f9eee50
--global <--global>
2023-06-02 14:20:26 -0300

    Modal component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Modal from "./components/modals/Modal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <Navbar />
+        <Modal isOpen actionLabel="Submit" />
         {children}
       </body>
     </html>
